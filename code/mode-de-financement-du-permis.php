cd9aa8f0b08b56d399765ba5a82d1046f0152a74
keithvald <73640852+keithvald@users.noreply.github.com>
2021-03-18 15:11:28 +0100

    final

---
+++ code/mode-de-financement-du-permis.php
@@ -0,0 +1,92 @@
+<!--header-->
+<?php include '../includes/header-page.php'; ?>
+
+<div class="landing-page-image-container">
+  <img src="../assets/img/permis/8.webp" alt="Conduite Accompagné photo" />
+</div>
+
+<!--Section JUMBOTRON-->
+<div class="container-fluid mt-2">
+  <div class="landing-page-description">
+    <div class="jumbotron row p-8">
+        <h1 class="">Mode de financement du permis</h1>
+        <div class="col-12 col-md-9">
+          <p class="c-blue lead font-weight-bold">
+            <h2>Mon compte formation</h2>
+          </p>
+          <p class="init">
+              Le <strong class="c-blue">permis</strong> est une formation <strong class="c-blue">éligible au CPF</strong> depuis l’amendement au projet de loi Egalité et Citoyenneté. À partir du 15 mars 2017, toutes personnes demandeurs d’emploi ou salariés peuvent mobiliser ses heures <strong class="c-blue">CPF</strong>  pour la préparation des épreuves du code et de la conduite du <strong class="c-blue">permis B</strong>. 
+              Cela signifie que vous pouvez financer partiellement ou totalement votre permis à l'aide de vos CPF selon ce que vous avez sur votre "compte personnel de formation".
+              Vous pouvez vérifier vos droits en créant votre compte sur le site <a href="www.moncompteformation.gouv.fr"><strong class="c-blue">www.moncompteformation.gouv.fr</strong></a>.
+              Dans un second temps il faudra relier votre compte à Franceconnect avec vos identifiants <a href="impot.gouv.fr"><strong class="c-blue">impot.gouv.fr</strong></a> ou de l'assurance maladie.
+              Par la suite nous vous conseillerons et vous guiderons dans votre choix sur la formule la plus adaptée à votre situation et à vos droits à la formation.
+          </p>
+        </div>
+        <div class="col-12 col-md-3 text-center">
+          <img src="../assets/img/illustrations/monnaie.jpg" alt="image monnaie compte_formation" class="d-none d-md-block img-monnaie-personne" width="300px">
+        </div>
+    </div>
+  </div>
+</div>
+
+<!--Facilités de paiement-->
+<div class="bg-custom-grey">
+    <div class="row p-8">
+      <div class="col-12">
+         <h1 class="text-left">Les falicités de paiement</h1>
+      </div>
+      <div class="col-12" >
+        Pour l'ensemble de permis nous proposons des facilités de paiement.
+        N'hésitez pas à nous contacter pour découvrir nos tarifs et nos facilités de paiement de base.
+        Nous pouvons aussi adapter les modalités de paiement à votre situation.   
+      </div>
+    </div>
+</div>
+<div class="bg-custom-blue">
+    <div class="row p-8">
+        <div class="col-12 col-md-5 col-xl-3 d-flex justify-content-around text-center">
+          <img src="../assets/img/illustrations/monnaie.png" alt="image personnes monnaie"
+          class="d-none d-md-block pictures-permis-auto-2" />
+        </div>
+        <div class="col-12 col-md-7 col-xl-9">
+          <h2 class="text-white text-center text-md-left">Lorem ipsum</h2>
+            <p class="livret text-white text-center text-md-left">Lors de votre inscription et après l’enregistrement de votre dossier en préfecture il vous sera remis votre <strong>livret d’apprentissage AAC</strong>.</p>
+        </div>
+    </div>
+</div>
+
+<!--Les aides Pôle Emploi-->
+<div class="container-fluid">
+    <div class="row p-8">
+        <div class="col-12 col-md-12">
+            <h2>Les aides financières de Pôle Emploi</h2>
+            <p class="aides">L'aide au permis ne peut être attribuée qu'une seule fois. Son montant peut s'élever <strong class="c-blue">jusqu'à un maximum de 1200€</strong>. 
+            Elle est directement versée par <strong class="c-blue">Pôle Emploi</strong> à l'auto-école et il faut consulter son conseiller pôle emploi pour savoir <strong class="c-blue">si l'on est éligible à cette aide</strong>.
+            Si c'est le cas, l'auto-école vous fera un devis que vous recevrez directement sur votre <strong class="c-blue">espace personnel Pôle Emploi</strong></p>
+            <br/>
+        </div> 
+    </div>
+</div>
+
+<!--Permis à un euro-->
+<div class="bg-custom-grey">
+  <div class="row p-8">
+    <div class="col-12">
+      <h1 class="left">Le permis à 1€/Jour</h1>
+    </div>
+        <div class="col-12 col-md-9">
+            <p class="init"><strong class="c-blue">Le "permis à un euro par jour"</strong> : c'est la possibilité d'étaler, sans aucun frais supplémentaire, le paiement de la formation au permis de conduire. Ce dispositif permet aux jeunes de <strong class="c-blue">15 à 25 ans</strong>, selon certaines conditions, de lisser le coût de la formation à <strong class="c-blue">la catégorie A et B du permis de conduire</strong> à raison d'un euro par jour. Il s'agit d'un prêt à taux zéro dont les intérêts sont pris en charge par l’État.</p>
+          <div id="mon-collapse" class="collapse">
+            <p>Pour bénéficier du permis à 1€ par jour, il faut faire un <strong class="c-blue">contrat de formation "permis 1€ par jour" à l'auto-école</strong> puis valider la demande auprès de sa banque./p>
+          </div>
+          <button class="code-interest" type="button" data-target="#mon-collapse" data-toggle="collapse" aria-expanded="false" aria-controls="#mon-collapse">Lire la suite</button>
+        </div>
+        <div class="col-12 col-md-3 text-center">
+          <img src="../assets/img/illustrations/mon.png" alt="image monnaie" class="d-none d-md-block img-monnaie" width="210px">
+        </div>
+    </div>
+</div>
+
+
+<!--footer-->
+<?php include '../includes/footer-page.php'; ?>
