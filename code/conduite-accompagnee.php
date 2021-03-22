c4aecc72b2fa3f954bd10b8748cfe4462194d36f
H4nk17 <74056277+H4nk17@users.noreply.github.com>
2021-03-22 10:02:04 +0100

    Update conduite-accompagnee.php

--- code/conduite-accompagnee.php
+++ code/conduite-accompagnee.php
@@ -114,7 +114,8 @@
   <div class="row p-8">
     <div class="col-12">
         <h1 class="text-left text-white">Le permis de conduire</h1>
-          <p class="text-white">Vous ne pourrez passer votre examen qu’au moins 12 mois après la délivrance de l’attestation de fin de formation initiale et à condition d’avoir <strong>au moins 17 ans et demi</strong> (il sera cependant possible de conduire seul qu’à partir de 18 ans).
+         <p class="text-white">Vous ne pourrez passer votre examen qu’au moins 12 mois après la délivrance de l’attestation de fin de formation initiale et à condition d’avoir <strong>au moins 17 ans</strong> (il sera cependant possible de conduire seul qu’à partir de 18 ans).
+          </p>
           </p>           
       </div>
   </div>
