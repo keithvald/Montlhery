809a74d393bd2cfa0c28523814038081b5b559b2
keithvald <73640852+keithvald@users.noreply.github.com>
2021-03-25 14:13:10 +0100

    leaflet map

--- index.php
+++ index.php
@@ -294,12 +294,10 @@
 
 <!--Map-->
 <div class="container-fluid">
-	<div class="col-12 section-map">
+	<div class="col-12 section">
 		<h1>Où nous trouver</h1>
+		<div id="map"></div>
 	</div>
-	<iframe
-		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2909.06611892999!2d3.0048772149990457!3d43.18712347913998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ac6e237240cd%3A0xfa638979dd755178!2s8%20Rue%20Ancienne%20Porte%20de%20B%C3%A9ziers%2C%2011100%20Narbonne!5e0!3m2!1sfr!2sfr!4v1611734652187!5m2!1sfr!2sfr"
-		class="map" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" title="map"></iframe>
 </div>
 
 <!--footer-->
